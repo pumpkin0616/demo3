e03dead6a5b5af1a4f2e08f930b63f824f7e2928
pumpkin0616 <61608531+pumpkin0616@users.noreply.github.com>
2024-12-20 14:44:59 +0800

    feat:设置项目端口

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  devServer: {
+    port: 3006, // 替换为您需要的端口号
+    open: true, // 启动时自动打开浏览器
+    host: 'localhost', // 设置主机名（默认是 localhost）
+    // proxy: { // 如果需要配置代理，可以添加此项
+    //   '/api': {
+    //     target: 'http://localhost:3000', // 后端服务器地址
+    //     changeOrigin: true,
+    //   },
+    // },
+  },
+  transpileDependencies: true,
+
 })
